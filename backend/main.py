8c2726809b646df49479568551768a30a63d13c2
Danila <daneelahkalgahnov@gmail.com>
2026-05-02 14:31:47 +0300

    feat: basic verification

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 
+VALID_EMAIL = "test@gmail.com"
+VALID_PASSWORD = "12345"
+
 app = FastAPI()
 
 class LoginForm(BaseModel):
@@ -9,4 +12,9 @@ class LoginForm(BaseModel):
 
 @app.post("/auth", status_code=200)
 async def root(form: LoginForm):
-    return {"status": "authenticate"}
+    if (form.email == VALID_EMAIL and form.password == VALID_PASSWORD):
+        return {"status": "ok"}
+    raise HTTPExecption(
+        status_code=status.HTTP_401_UNAUTHRIZED,
+        detail="Invalid email or password"
+    )
