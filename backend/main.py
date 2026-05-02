d11df935e2cf9d09022bcd3a8ac8be073f575951
Danila <daneelahkalgahnov@gmail.com>
2026-05-02 13:43:58 +0300

    feat: login form

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,12 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
+class LoginForm(BaseModel):
+    email: str
+    password: str
 
-@app.get("/auth", status_code=200)
-async def root():
+@app.post("/auth", status_code=200)
+async def root(form: LoginForm):
     return {"status": "authenticate"}
